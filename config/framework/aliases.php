ef2c195d2b4b8e60e17f855a74bb9ed463b1ad4a
Mohammed Shifreen <mshifreen@gmail.com>
2022-12-26 09:21:21 +0530

    1. Aliases paths were updated properly. 2. Removed dev-tools appeared as twice in the composer.json repositories section. 3. Dockerfile updated for docker app service.

--- config/framework/aliases.php
+++ config/framework/aliases.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-Yii::setAlias('@OneCMS', dirname(__DIR__) . '/vendor/getonecms');
-Yii::setAlias('@App', dirname(__DIR__));
-Yii::setAlias('@Extensions', dirname(__DIR__) . '/extensions');
-Yii::setAlias('@Themes', dirname(__DIR__) . '/themes');
+Yii::setAlias('@OneCMS', dirname(__DIR__, 2) . '/vendor/getonecms');
+Yii::setAlias('@App', dirname(__DIR__, 2));
+Yii::setAlias('@Extensions', dirname(__DIR__, 2) . '/extensions');
+Yii::setAlias('@Themes', dirname(__DIR__, 2) . '/themes');
--- config/framework/web.php
+++ config/framework/web.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use OneTen\Theme;
+use OneTwenty\Theme;
 
 require __DIR__ . '/aliases.php';
 
