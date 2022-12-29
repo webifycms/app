8ae6248135a14d559742cac15a61ce4a89fa2382
Mohammed Shifreen <mshifreen@gmail.com>
2022-12-29 10:10:30 +0530

    1. Depedencies were upgraded. 2. Package yii2-gii removed and config files were adjusted accordingly.

--- src/Presentation/Web/Front/Controller/SiteController.php
+++ src/Presentation/Web/Front/Controller/SiteController.php
@@ -7,33 +7,24 @@ namespace App\Presentation\Web\Front\Controller;
 use yii\web\Controller;
 
 /**
- * Class SiteController
- *
- * @package getonecms/app
- * @version 0.0.1
- * @since   0.0.1
- * @author  Mohammed Shifreen
+ * Class SiteController.
  */
 class SiteController extends Controller
 {
+	/**
+	 * @return array<mixed>
+	 */
+	public function actions(): array
+	{
+		return [
+			'error' => [
+				'class' => 'yii\web\ErrorAction',
+			],
+		];
+	}
 
-    /**
-     * @return array
-     */
-    public function actions(): array
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ]
-        ];
-    }
-
-    /**
-     * @return string
-     */
-    public function actionIndex(): string
-    {
-        return $this->render('index');
-    }
+	public function actionIndex(): string
+	{
+		return $this->render('index');
+	}
 }
