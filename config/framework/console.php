a8b3a47c4cbee0a80aa18085733c443453fdfca9
Mohammed Shifreen <mshifreen@gmail.com>
2023-05-07 13:02:52 +0530

    Rebranded, composer.json and dependecies were updated, files header updated and console scripts renamed to webify.

--- config/framework/console.php
+++ config/framework/console.php
@@ -1,5 +1,13 @@
 <?php
-
+/**
+ * The file is part of the "webifycms/app", WebifyCMS application.
+ *
+ * @see https://webifycms.com/
+ *
+ * @copyright Copyright (c) 2023 WebifyCMS
+ * @license https://webifycms.com/license
+ * @author Mohammed Shifreen <mshifreen@gmail.com>
+ */
 declare(strict_types=1);
 
 use yii\console\controllers\MigrateController;
@@ -12,7 +20,7 @@ $config = [
 	'id'                  => 'console',
 	'basePath'            => dirname(__DIR__),
 	'bootstrap'           => ['log'],
-	'controllerNamespace' => 'App\Presentation\Console\Controller',
+	'controllerNamespace' => 'Webify\Presentation\Console\Controller',
 	'components'          => [
 		'cache' => [
 			'class' => 'yii\caching\FileCache',
