5c8252040998c9d972f6efebfaabbb19e29e364e
Mohammed Shifreen <mshifreen@gmail.com>
2023-05-07 13:02:52 +0530

    Rebranded, composer.json and dependecies were updated, files header updated and console scripts renamed to webify.

--- config/console.php
+++ config/console.php
@@ -1,11 +1,21 @@
 <?php
+/**
+ * The file is part of the "webifycms/app", WebifyCMS application.
+ *
+ * @see https://webifycms.com/
+ *
+ * @copyright Copyright (c) 2023 WebifyCMS
+ * @license https://webifycms.com/license
+ * @author Mohammed Shifreen <mshifreen@gmail.com>
+ */
+declare(strict_types=1);
 
 // import framework configurations
 $framework = require __DIR__ . '/framework/console.php';
 
 return [
-    'framework' => $framework,
-    'bootstrap' => [
-        OneCMS\User\ConsoleBootstrap::class
-    ],
+	'framework' => $framework,
+	'bootstrap' => [
+		Webify\User\ConsoleBootstrap::class,
+	],
 ];
--- public/index.php
+++ public/index.php
@@ -1,5 +1,21 @@
 <?php
+/**
+ * The file is part of the "webifycms/app", WebifyCMS application.
+ *
+ * @see https://webifycms.com/
+ *
+ * @copyright Copyright (c) 2023 WebifyCMS
+ * @license https://webifycms.com/license
+ * @author Mohammed Shifreen <mshifreen@gmail.com>
+ */
+declare(strict_types=1);
+
 // should require the composer autoloader on first
+use function Webify\Base\Infrastructure\app;
+use function Webify\Base\Infrastructure\configure;
+use function Webify\Base\Infrastructure\enable_dev_env;
+use function Webify\Base\Infrastructure\load_env_variables;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 // comment out or delete the following line when deployed to production
