b7cbd02f60e7070a3df3e0de70673e0588d4e272
Mohammed Shifreen <mshifreen@gmail.com>
2023-05-30 23:34:21 +0530

    Updated webifycms packages and made some adjustments accordingly.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -13,13 +13,18 @@ declare(strict_types=1);
 // should require the composer autoloader on first
 require __DIR__ . '/vendor/autoload.php';
 
-use Webify\Tools\Fixer;
 use PhpCsFixer\Finder;
+use Webify\Tools\Fixer;
 
 $finder = Finder::create()
 	->in([
 		__DIR__ . '/src',
 		__DIR__ . '/test',
+	])
+	->exclude([
+		__DIR__ . '/extensions',
+		__DIR__ . '/themes',
+		__DIR__ . '/vendor',
 	])
 	->name('*.php')
 ;
