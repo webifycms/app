1f9022936e62d290509085b20becca77f0d97bc1
Mohammed Shifreen <mshifreen@gmail.com>
2024-03-26 01:10:42 +0530

    Changes made according to the base extension changes regarding enabling debug mode.

--- public/index.php
+++ public/index.php
@@ -14,20 +14,22 @@ use Webify\Base\Infrastructure\Service\Application\WebApplicationService;
 use Webify\Base\Infrastructure\Service\Config\ConfigService;
 
 // should require the composer autoloader on first
+use function Webify\Base\Infrastructure\define_environment;
 use function Webify\Base\Infrastructure\dependency;
-use function Webify\Base\Infrastructure\enable_debug;
+use function Webify\Base\Infrastructure\get_env_variable;
 use function Webify\Base\Infrastructure\load_env_variables;
 
+// load composer autoloader
 require __DIR__ . '/../vendor/autoload.php';
-
-// comment out or delete the following line when deployed to production
-enable_debug();
 
 // load Yii class file
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 // load the env variables
 load_env_variables(dirname(__DIR__));
+
+// comment out or delete the following line when deployed to production, by default the environment set as production.
+define_environment(get_env_variable('APP_ENVIRONMENT'), get_env_variable('APP_DEBUG') === 'true' ? true : false);
 
 // load default configurations
 $config = require __DIR__ . '/../config/web.php';
